127fa194e4d3aa5f3c0ab02296f57549789a7f72
haski1 <perepelkin2601@gmail.com>
2019-03-06 19:40:40 +0500

    Add class map

---
+++ VoidTime/Logic/GameMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidTime
+{
+    class GameMap
+    {
+        #region Private Fields
+
+        private readonly int chunks;
+        private readonly int chunkSize;
+
+        #endregion
+
+        #region Public Properties
+
+        public int Size { get; }
+
+        #endregion
+
+
+        public GameMap()
+        {
+
+        }
+    }
+}
