e8d6ff98c3b30715853db14c750ca9910d4d8e44
haski1 <perepelkin2601@gmail.com>
2019-03-06 19:22:41 +0500

    Add class GameObject

---
+++ VoidTime/GameObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidTime
+{
+    class GameObject
+    {
+        #region Public Properties
+
+        public string NameObject { get; }
+        public PointF Position { get; set; }
+        public byte DrawingPriority { get; }
+
+        #endregion
+        
+        #region Constructor
+
+        public GameObject(string nameObject)
+        {
+            NameObject = nameObject;
+            DrawingPriority = 0;
+            Position = new PointF();
+        }
+
+        public GameObject(string nameObject, PointF position)
+        {
+            NameObject = nameObject;
+            DrawingPriority = 0;
+            Position = position;
+        }
+
+        public GameObject(string nameObject, PointF position, byte drawingPriority)
+        {
+            NameObject = nameObject;
+            DrawingPriority = drawingPriority;
+            Position = position;
+        }
+
+        #endregion
+
+    }
+}
