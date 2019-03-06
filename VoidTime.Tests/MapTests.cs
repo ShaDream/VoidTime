abbdbbd486ea540e9fcea6449beca44846a26f9a
ShaDream <russianboysasha@yandex.ru>
2019-03-06 22:08:36 +0500

    Add Test Project

---
+++ VoidTime.Tests/MapTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using VoidTime;
+
+namespace VoidTime.Tests
+{
+    [TestFixture]
+    public class MapTests
+    {
+        [Test]
+        public void With2x2ChunksWithoutGameObjects()
+        {
+            var map = new GameMap(new Size(2, 2), new Size(100, 100));
+        }
+
+        [Test]
+        public void WithSomeRightObjects()
+        {
+            var objects = new[] { new GameObject("Player"), new GameObject("Planet", new Vector2D(5, 5)), };
+            var map = new GameMap(new Size(2, 2), new Size(100, 100), objects);
+        }
+
+        [Test]
+        public void WithSomeNotRightObjects()
+        {
+            var objects = new[] { new GameObject("Player"), new GameObject("Planet", new Vector2D(10005, 10005)), };
+            try
+            {
+                new GameMap(new Size(2, 2), new Size(100, 100), objects);
+                Assert.Fail("Should Arguments out of range");
+            }
+            catch { }
+        }
+    }
+}
---
+++ VoidTime.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VoidTime.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VoidTime.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a6b46a2-6c5b-47ce-80c4-46a47fc4192e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
