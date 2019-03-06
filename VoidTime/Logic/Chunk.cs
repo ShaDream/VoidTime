9abe1f32b6e7ebd023648c1d0562eefff0b1d00a
ShaDream <russianboysasha@yandex.ru>
2019-03-06 19:32:51 +0500

    Add base Chunk, GameModel, Vector2D

---
+++ VoidTime/Logic/Chunk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace VoidTime
+{
+    public class Chunk
+    {
+        public Point Coordinates { get; private set; }
+        public Size Size { get; private set; }
+
+        private List<GameObject> GameObjects = new List<GameObject>();
+
+        public void AddGameObject()
+        {
+
+        }
+
+        public void AddGameObjects()
+        {
+
+        }
+
+        public event Action<GameObject> GameObjectOutOfChunk;
+
+    }
+}
