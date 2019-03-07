d75ca5f885da9bc726eb202a3fc61ea5c1493ffb
ShaDream <russianboysasha@yandex.ru>
2019-03-08 01:05:15 +0500

    Add basic WinFormsPainting

--- VoidTime/MainForm.cs
+++ VoidTime/MainForm.cs
@@ -1,12 +1,42 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace VoidTime
 {
     public class MainForm : Form
     {
-        public MainForm()
-        {
+        public List<Vector2D> DrawObjects = new List<Vector2D>();
+        public Image plane = new Bitmap(@"D:\Downloads\UFO.png");
 
+        public MainForm(GameModel model)
+        {
+            DoubleBuffered = true;
+            Size = new Size(800, 800);
+
+            model.Tick += FrameTick;
+
+            KeyUp += model.OnKeyRelease;
+            KeyDown += model.OnKeyPress;
+
+
+            model.Run();
+        }
+
+
+        private void FrameTick(List<GameObject> objectsToDraw, List<Vector2D> position)
+        {
+            DrawObjects = position;
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var pen = new Pen(Color.Black);
+            foreach (var drawObject in DrawObjects)
+            {
+                e.Graphics.DrawImage(plane, drawObject.X, drawObject.Y, 100, 50);
+            }
         }
     }
 }
--- VoidTime/Program.cs
+++ VoidTime/Program.cs
@@ -13,7 +13,7 @@ namespace VoidTime
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new MainForm(new GameModel()));
         }
     }
 }
