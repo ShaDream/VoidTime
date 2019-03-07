f69ea986e2012935703f2a356efc2a91a0bbe1fa
haski1 <perepelkin2601@gmail.com>
2019-03-07 21:44:57 +0500

    Create class Camera

---
+++ VoidTime/Logic/Camera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace VoidTime
+{
+    public class Camera
+    {
+
+        #region Public Properties
+
+        public Vector2D Position { get; private set; }
+        public Size Size { get; set; }
+        public GameObject FollowTo { get; set; }
+
+        #endregion
+
+        #region Conctructor
+
+        public Camera(GameObject followTo, Size size)
+        {
+            Position = followTo.Position;
+            Size = size;
+            FollowTo = followTo;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public List<Vector2D> ToVectors()
+        {
+            var topLeftPoint = new Vector2D(Position.X - Size.Width / 2, Position.Y + Size.Height / 2);
+            var topRightPoint = new Vector2D(Position.X + Size.Width / 2, Position.Y + Size.Height / 2);
+            var bottomLeftPoint = new Vector2D(Position.X - Size.Width / 2, Position.Y - Size.Height / 2);
+            var bottomRightPoint = new Vector2D(Position.X - Size.Width / 2, Position.Y - Size.Height / 2);
+            return new List<Vector2D> { topLeftPoint, topRightPoint, bottomLeftPoint, bottomRightPoint };
+        }
+
+        public void Update()
+        {
+            Position = FollowTo.Position;
+        }
+
+        #endregion
+    }
+}
