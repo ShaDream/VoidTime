8f4bd7c58d19b537d1b09140aaeb43cf8af8a320
ShaDream <russianboysasha@yandex.ru>
2019-03-09 15:30:34 +0500

    Change properties

--- VoidTime.Generator/Properties/AssemblyInfo.cs
+++ VoidTime.Generator/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VoidTime.Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("gypnor")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VoidTime.Generator")]
-[assembly: AssemblyCopyright("Copyright © gypnor 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
