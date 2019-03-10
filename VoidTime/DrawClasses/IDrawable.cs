20ff1fa597bf6a1559406d2be0ff47faae9e4785
ShaDream <russianboysasha@yandex.ru>
2019-03-11 00:14:44 +0500

    Add IDrawabe inteface

---
+++ VoidTime/DrawClasses/IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+namespace VoidTime
+{
+    public interface IDrawable
+    {
+        Type GameObjectType { get; }
+
+        void DrawObject(ObjectOnDisplay obj, Graphics graphics);
+    }
+}
