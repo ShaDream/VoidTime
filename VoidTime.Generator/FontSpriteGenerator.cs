0018ee4cd14f9054c74a5b36b0f937e9e5291f9d
ShaDream <russianboysasha@yandex.ru>
2019-03-17 12:50:48 +0500

    Add basic FontDataGenerator

---
+++ VoidTime.Generator/FontSpriteGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Windows.Media;
+using Color = System.Drawing.Color;
+
+namespace VoidTime.Generator
+{
+    public class FontSpriteGenerator
+    {
+        public string ImagePath;
+        public int CellSize;
+        public int WidthCount;
+        public int OriginLine;
+        public string characters;
+
+        public void GenerateDataSettings()
+        {
+            var bitmap = new Bitmap(ImagePath);
+
+            NumberFormatInfo nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+
+            var builder = new StringBuilder();
+            builder.Append(
+                "private static Dictionary<char, Character> characters = new Dictionary<char, Character>\r\n        {");
+
+            for (var i = 0; i < characters.Length; i++)
+            {
+                var character = characters[i];
+                var xCount = i % WidthCount;
+                var yCount = i / WidthCount;
+                var baseLine = yCount * CellSize + OriginLine;
+                var xMin = CellSize;
+                var xMax = 0;
+                var yMin = CellSize;
+                var yMax = 0;
+
+                for (int y = 0; y < CellSize; y++)
+                    for (int x = 0; x < CellSize; x++)
+                    {
+                        var color = bitmap.GetPixel(xCount * CellSize + x, yCount * CellSize + y);
+                        if (color.A != 0)
+                        {
+                            xMin = Math.Min(xMin, x);
+                            xMax = Math.Max(xMax, x);
+                            yMin = Math.Min(yMin, y);
+                            yMax = Math.Max(yMax, y);
+                        }
+                    }
+
+                var originOffset =  yMax - OriginLine;
+                originOffset = originOffset > 0 ? originOffset : 0;
+                SizeF size = new SizeF(xMax - xMin, yMax - yMin);
+                PointF atlasOrigin = new PointF((float)(xCount * CellSize + xMin) / bitmap.Size.Width,
+                    (float)(yCount * CellSize + yMax) / bitmap.Size.Height);
+
+                SizeF atlasSize = new SizeF((float)(xCount * CellSize + xMax) / bitmap.Size.Width - atlasOrigin.X,
+                    atlasOrigin.Y - (float)(yCount * CellSize + yMin) / bitmap.Size.Height);
+
+                builder.Append(
+                    $"{{'{character}',new Character('{character}', new SizeF({size.Width}, {size.Height}), {originOffset}, 2, new Vector2D({atlasOrigin.X.ToString(nfi)}f, {atlasOrigin.Y.ToString(nfi)}f), new SizeF({atlasSize.Width.ToString(nfi)}f, {atlasSize.Height.ToString(nfi)}f)) }},\n");
+            }
+
+            builder.Append("        };");
+            using (var output = new StreamWriter("D:\\text.txt"))
+            {
+                output.Write(builder.ToString());
+            }
+        }
+
+    }
+}
--- VoidTime.Generator/MainForm.cs
+++ VoidTime.Generator/MainForm.cs
@@ -23,14 +23,33 @@ namespace VoidTime.Generator
             var menu = new MenuStrip { Height = 30 };
             var generatorMenuItem = new ToolStripMenuItem("Generator");
             var createBackgroundGenerator = new ToolStripMenuItem("Create background image");
+            var createFontSpriteGenerator = new ToolStripMenuItem("Create Font Sprite");
             generatorMenuItem.DropDownItems.Add(createBackgroundGenerator);
+            generatorMenuItem.DropDownItems.Add(createFontSpriteGenerator);
 
             menu.Items.Add(generatorMenuItem);
 
             createBackgroundGenerator.Click += CreateBackgroundGeneratorPage;
+            createFontSpriteGenerator.Click += CreateFontSpriteGeneratorPage;
 
             grid.Controls.Add(menu, 0, 0);
             grid.Controls.Add(panel, 0, 1);
+
+            var s = new FontSpriteGenerator();
+            s.CellSize = 83;
+            s.ImagePath = @"C:\Users\ShaDream\source\repos\VoidTime\VoidTime.Resources\Resources\EuropeFont.png";
+            s.OriginLine = 65;
+            s.WidthCount = 24;
+            s.characters =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,;:?!-_~#\"\'&()[]|`\\/@°+=*$£€<>%АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            s.GenerateDataSettings();
+        }
+
+        private void CreateFontSpriteGeneratorPage(object sender, System.EventArgs e)
+        {
+            ClearPanel();
+
+
         }
 
         private void CreateBackgroundGeneratorPage(object sender, System.EventArgs e)
