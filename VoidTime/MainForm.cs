e65e5be8753f0c9b66f9ee0575ac8fba536ff7ed
ShaDream <russianboysasha@yandex.ru>
2019-03-19 12:35:49 +0500

    add Init method into IDrawable

--- VoidTime/MainForm.cs
+++ VoidTime/MainForm.cs
@@ -1,42 +1,19 @@
 ﻿using System;
-using System.Drawing;
-using System.Windows.Forms;
 using System.Collections.Generic;
-using System.Drawing.Imaging;
+using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
-using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using SharpGL;
 using SharpGL.Enumerations;
-using SharpGL.SceneGraph.Assets;
 
 namespace VoidTime
 {
     public class MainForm : Form
     {
-        #region Public Properties
-
-        private DateTime lastTime;
-        private readonly OpenGLControl openGL;
-        private int framesRendered;
-        private int fps;
-        private Dictionary<Type, Action<ObjectOnDisplay, OpenGL>> drawHelpers =
-            new Dictionary<Type, Action<ObjectOnDisplay, OpenGL>>();
-
-        private List<ObjectOnDisplay> DrawObjects = new List<ObjectOnDisplay>();
-
-        public static Texture player = new Texture();
-        public static Texture background = new Texture();
-        public static Texture mars = new Texture();
-
-        #endregion
-
         #region Constructor
 
         public MainForm(GameModel model)
         {
-            HelperInitialization();
-
             openGL = new OpenGLControl
             {
                 FrameRate = 60,
@@ -46,12 +23,14 @@ namespace VoidTime
                 DrawFPS = false
             };
 
-            ((System.ComponentModel.ISupportInitialize)(openGL)).BeginInit();
+            HelperInitialization();
+
+            ((ISupportInitialize) openGL).BeginInit();
             openGL.OpenGLDraw += OpenGLDraw;
             openGL.OpenGLInitialized += OpenGL_OpenGLInitialized;
 
             Controls.Add(openGL);
-            ((System.ComponentModel.ISupportInitialize)(openGL)).EndInit();
+            ((ISupportInitialize) openGL).EndInit();
 
             WindowState = FormWindowState.Maximized;
             ShowIcon = false;
@@ -61,22 +40,30 @@ namespace VoidTime
             openGL.KeyDown += model.OnKeyPress;
             SizeChanged += (s, a) => model.GameBasicCamera.Size = Size;
             model.GameBasicCamera.Size = Size;
-            
+
             model.Run();
         }
+
+        #endregion
+
+        #region Public Properties
+
+        private readonly OpenGLControl openGL;
+
+        private readonly Dictionary<Type, Action<ObjectOnDisplay, OpenGL>> drawHelpers =
+            new Dictionary<Type, Action<ObjectOnDisplay, OpenGL>>();
+
+        private List<ObjectOnDisplay> DrawObjects = new List<ObjectOnDisplay>();
+
+        #endregion
+
+        #region Private Methods
 
         private void OpenGL_OpenGLInitialized(object sender, EventArgs e)
         {
             var gl = openGL.OpenGL;
 
             gl.Enable(OpenGL.GL_TEXTURE_2D);
-
-            player.Create(openGL.OpenGL,
-                new Bitmap(@"C:\Users\Иван\Desktop\TEXTURES\delodean.png"));
-            background.Create(openGL.OpenGL,
-                new Bitmap(@"C:\Users\Иван\Desktop\TEXTURES\nebo.jpg"));
-            mars.Create(openGL.OpenGL,
-                new Bitmap(@"C:\Users\Иван\Desktop\TEXTURES\neptune1.png"));
         }
 
         private void OpenGLDraw(object sender, RenderEventArgs args)
@@ -90,35 +77,36 @@ namespace VoidTime
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
             gl.MatrixMode(MatrixMode.Projection);
             gl.LoadIdentity();
-            gl.Ortho(0,openGL.Width,openGL.Height, 0, 0.01, -100);
+            gl.Ortho(0, openGL.Width, openGL.Height, 0, 0.01, -100);
 
             gl.MatrixMode(MatrixMode.Modelview);
 
             foreach (var objectOnDisplay in DrawObjects)
-                drawHelpers[objectOnDisplay.GameObject.GetType()].Invoke(objectOnDisplay,gl);
+                drawHelpers[objectOnDisplay.GameObject.GetType()].Invoke(objectOnDisplay, gl);
 
             gl.Disable(OpenGL.GL_BLEND);
 
             gl.Flush();
         }
 
-        #endregion
-
-        #region Private Methods
-
         private void HelperInitialization()
         {
-            foreach (var drawClass in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
-                .Where(x => typeof(IDrawable).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract))
+            foreach (var drawClass in AppDomain.CurrentDomain.GetAssemblies()
+                                               .SelectMany(x => x.GetTypes())
+                                               .Where(x => typeof(IDrawable).IsAssignableFrom(x) &&
+                                                           !x.IsInterface &&
+                                                           !x.IsAbstract))
             {
                 var instance = Activator.CreateInstance(drawClass);
-                var typeGameObject = (Type)drawClass.GetProperty("GameObjectType").GetValue(instance, null);
-                var drawingMethod = (Action<ObjectOnDisplay, OpenGL>)drawClass.GetMethod("DrawObject")
-                    .CreateDelegate(typeof(Action<ObjectOnDisplay, OpenGL>), instance);
+                ((IDrawable) instance).Init(openGL.OpenGL);
+                var typeGameObject = (Type) drawClass.GetProperty("GameObjectType").GetValue(instance, null);
+                var drawingMethod = (Action<ObjectOnDisplay, OpenGL>) drawClass.GetMethod("DrawObject")
+                                                                               .CreateDelegate(
+                                                                                   typeof(Action<ObjectOnDisplay,
+                                                                                       OpenGL>), instance);
 
                 drawHelpers.Add(typeGameObject, drawingMethod);
             }
-
         }
 
         private void FrameTick(List<GameObject> objectsToDraw, BasicCamera gameBasicCamera)
@@ -128,24 +116,6 @@ namespace VoidTime
                            select new ObjectOnDisplay(objectToDraw, positionOnDisplay)).ToList();
         }
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            //e.Graphics.DrawString($"fps {fps.ToString()}", new Font("Arial", 40, FontStyle.Bold),
-            //    new SolidBrush(Color.Yellow), 0, 0);
-            //Update();
-        }
-
-        private void Update()
-        {
-            framesRendered++;
-
-            if (!((DateTime.Now - lastTime).TotalSeconds >= 1)) return;
-            fps = framesRendered;
-            framesRendered = 0;
-            lastTime = DateTime.Now;
-        }
-
         #endregion
-
     }
 }
