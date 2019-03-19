fb34d47d299a4eea4002ae7c5f8a0138ff55392c
ShaDream <russianboysasha@yandex.ru>
2019-03-19 13:02:35 +0500

    Add IGameModel Interface

---
+++ VoidTime/Logic/IGameModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoidTime
+{
+    public interface IGameModel
+    {
+        void Run();
+        void Pause();
+
+        event Action<List<GameObject>, BasicCamera> Tick;
+        event Action<IGameModel> GameModelChanged;
+    }
+}
