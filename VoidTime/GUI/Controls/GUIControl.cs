25f1429d765dec144dbff8f872ca48d209fcf20e
ShaDream <russianboysasha@yandex.ru>
2019-03-23 18:34:02 +0500

    Add GuiControl properties

--- VoidTime/GUI/Controls/GUIControl.cs
+++ VoidTime/GUI/Controls/GUIControl.cs
@@ -9,5 +9,10 @@ namespace VoidTime.GUI
         public Size Size { get; set; }
 
         public int DrawIndex { get; set; }
+
+        public bool Enabled { get; set; }
+        public bool Visible { get; set; }
+
+        public UIMask Mask { get; set; }
     }
 }
