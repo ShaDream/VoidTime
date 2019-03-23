54a6c95011db36f6c539f57bd8957c6d3c710e72
ShaDream <russianboysasha@yandex.ru>
2019-03-23 18:36:50 +0500

    Add WindowFactory

---
+++ VoidTime/GUI/GUIFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VoidTime.GUI
+{
+    public static class GUIFactory
+    {
+        public static UIWindow Create(IGameModel model)
+        {
+            switch (model)
+            {
+                case GameModel _:
+                    var MainGameUI = new MainGameUI(model as GameModel);
+                    return MainGameUI;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
