5bf5f901caa8c42c4a65ecac3d6d473cb8ac2492
ShaDream <russianboysasha@yandex.ru>
2019-03-23 18:35:22 +0500

    Add font Folder

---
+++ VoidTime/GUI/Font/FontAtlas.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using SharpGL;
+using SharpGL.SceneGraph.Assets;
+
+namespace VoidTime.GUI
+{
+    public class FontAtlas
+    {
+        private Dictionary<char, Character> Characters = new Dictionary<char, Character>();
+        public Bitmap Atlas { get; }
+        private Texture texture { get; set; } = new Texture();
+        private bool first = true;
+        public int FontSize { get; }
+        public float LineSpacing { get; }
+        public float Spacing { get; }
+
+
+        public FontAtlas(Dictionary<char, Character> characters,
+            Bitmap atlas,
+            int fontSize,
+            float lineSpacing,
+            float spacing)
+        {
+            Atlas = atlas;
+            Characters = characters;
+            FontSize = fontSize;
+            LineSpacing = lineSpacing;
+            Spacing = spacing;
+        }
+
+        public Texture GetTexture(OpenGL gl)
+        {
+            if (first)
+            {
+                texture.Create(gl, Atlas);
+                first = false;
+            }
+
+            return texture;
+        }
+
+        public Character GetCharacter(char ch)
+        {
+            return Characters?[ch];
+        }
+    }
+}
