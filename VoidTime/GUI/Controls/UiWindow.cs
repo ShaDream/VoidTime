26dcdadc63401cda18c4c5305fe09d9699171107
ShaDream <russianboysasha@yandex.ru>
2019-03-23 18:36:12 +0500

    Add basic logic to UIWindow

--- VoidTime/GUI/Controls/UiWindow.cs
+++ VoidTime/GUI/Controls/UiWindow.cs
@@ -1,9 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace VoidTime.GUI
 {
-    public class UIWindow
+    public abstract class UIWindow : GUIControl
     {
         public List<GUIControl> Controls = new List<GUIControl>();
+
+        public abstract void Unsubscribe();
+
+        public abstract void OnKeyPress(object sender, KeyEventArgs args);
+        public abstract void OnKeyRelease(object sender, KeyEventArgs args);
+        public abstract void OnMouseWheel(object sender, MouseEventArgs args);
+        public abstract void OnMouseMove(object sender, MouseEventArgs args);
+        public abstract void OnMouseClick(object sender, MouseEventArgs args);
+        public abstract void OnMouseDoubleClick(object sender, MouseEventArgs args);
+        public abstract void OnSizeChanged(object sender, EventArgs args);
+
+        public event Action<List<IDrawData>> UIChanged;
+
+        public void OnUIChanged(List<IDrawData> data)
+        {
+            UIChanged?.Invoke(data);
+        }
     }
 }
