13f2a95670e2478068045ae594d79dbc34392ced
ShaDream <russianboysasha@yandex.ru>
2019-03-24 12:26:11 +0500

    Add List with events OnAdd and OnRemove

---
+++ VoidTime/GUI/EventList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoidTime.GUI
+{
+    public class EventList<T> 
+    {
+        private List<T> objects = new List<T>();
+
+        public void Add(T item)
+        {
+            objects.Add(item);
+            ItemAdded?.Invoke(item);
+        }
+
+        public void Remove(T item)
+        {
+            objects.Remove(item);
+            ItemRemoved?.Invoke(item);
+        }
+
+        public event Action<T> ItemAdded;
+        public event Action<T> ItemRemoved;
+    }
+}
