d517eb13b6682c0e9c33c2d7ee308159ce99974f
ShaDream <russianboysasha@yandex.ru>
2019-03-24 12:25:06 +0500

    Add base draw rectangle

--- VoidTime/GUI/Controls/TextBox.cs
+++ VoidTime/GUI/Controls/TextBox.cs
@@ -1,11 +1,35 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace VoidTime.GUI
 {
     public class TextBox : GUIControl
     {
-        public string Text;
-        public Color Background;
+        public string Text { get; set; }
+        public Color Background { get; set; }
         public FontSettings Font { get; set; }
+
+        public override List<IDrawData> Draw()
+        {
+            List<IDrawData> data = new List<IDrawData>();
+            if (Background != Color.Transparent)
+            {
+                data.Add(new RectangleDrawData
+                {
+                    Color = Background,
+                    DrawPriority = DrawIndex,
+                    Mask = Mask,
+                    Points = new[]
+                    {
+                        new Vector2D(Location.X,Location.Y),
+                        new Vector2D(Location.X+Size.Width,Location.Y),
+                        new Vector2D(Location.X+Size.Width,Location.Y + Size.Height),
+                        new Vector2D(Location.X,Location.Y+ Size.Height),
+                    }
+                });
+            }
+
+            return data;
+        }
     }
 }
