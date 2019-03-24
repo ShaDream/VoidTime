e8f04980a632248b87015b831682f5fb0334c19c
ShaDream <russianboysasha@yandex.ru>
2019-03-24 12:24:36 +0500

    Delete key and mouse events

--- VoidTime/GUI/Controls/UiWindow.cs
+++ VoidTime/GUI/Controls/UiWindow.cs
@@ -6,23 +6,10 @@ namespace VoidTime.GUI
 {
     public abstract class UIWindow : GUIControl
     {
-        public List<GUIControl> Controls = new List<GUIControl>();
+        public EventList<GUIControl> Controls = new EventList<GUIControl>();
+
+        public Dictionary<GUIControl,List<IDrawData>> drawData = new Dictionary<GUIControl, List<IDrawData>>();
 
         public abstract void Unsubscribe();
-
-        public abstract void OnKeyPress(object sender, KeyEventArgs args);
-        public abstract void OnKeyRelease(object sender, KeyEventArgs args);
-        public abstract void OnMouseWheel(object sender, MouseEventArgs args);
-        public abstract void OnMouseMove(object sender, MouseEventArgs args);
-        public abstract void OnMouseClick(object sender, MouseEventArgs args);
-        public abstract void OnMouseDoubleClick(object sender, MouseEventArgs args);
-        public abstract void OnSizeChanged(object sender, EventArgs args);
-
-        public event Action<List<IDrawData>> UIChanged;
-
-        public void OnUIChanged(List<IDrawData> data)
-        {
-            UIChanged?.Invoke(data);
-        }
     }
 }
