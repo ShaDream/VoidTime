5ce0e6c753d2d1170360ac840e6225e89872298d
ShaDream <russianboysasha@yandex.ru>
2019-03-24 12:24:14 +0500

    Add key and mouse events

--- VoidTime/GUI/Controls/GUIControl.cs
+++ VoidTime/GUI/Controls/GUIControl.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace VoidTime.GUI
 {
@@ -14,5 +17,25 @@ namespace VoidTime.GUI
         public bool Visible { get; set; }
 
         public UIMask Mask { get; set; }
+
+        public virtual void OnKeyPress(object sender, KeyEventArgs args) { }
+        public virtual void OnKeyRelease(object sender, KeyEventArgs args) { }
+        public virtual void OnMouseWheel(object sender, MouseEventArgs args) { }
+        public virtual void OnMouseMove(object sender, MouseEventArgs args) { }
+        public virtual void OnMouseClick(object sender, MouseEventArgs args) { }
+        public virtual void OnMouseDoubleClick(object sender, MouseEventArgs args) { }
+        public virtual void OnSizeChanged(object sender, EventArgs args) { }
+
+        public event Action<GUIControl, List<IDrawData>> UIChanged;
+
+        public void OnUIChanged(GUIControl control, List<IDrawData> data)
+        {
+            UIChanged?.Invoke(control, data);
+        }
+
+        public virtual List<IDrawData> Draw()
+        {
+            return new List<IDrawData>();
+        }
     }
 }
