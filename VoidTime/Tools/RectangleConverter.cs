5339eac33b626e7f32be5a4231e2ba9b0618e80a
ShaDream <russianboysasha@yandex.ru>
2019-03-29 17:44:07 +0500

    ConverterTools added

---
+++ VoidTime/Tools/RectangleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace VoidTime
+{
+    public static class RectangleConverter
+    {
+        public static Vector2D[] ToVector2DPoints(Rectangle rect)
+        {
+            return ToVector2DPoints((RectangleF)rect);
+        }
+
+        public static Vector2D[] ToVector2DPoints(RectangleF rect)
+        {
+            return new[]
+            {
+                new Vector2D(rect.X, rect.Y),
+                new Vector2D(rect.X + rect.Width, rect.Y),
+                new Vector2D(rect.X + rect.Width, rect.Y + rect.Height),
+                new Vector2D(rect.X, rect.Y + rect.Height),
+            };
+        }
+    }
+}
