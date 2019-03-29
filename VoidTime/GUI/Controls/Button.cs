6914f4d79e65772fecf1ef87943f01bf6b5cf501
ShaDream <russianboysasha@yandex.ru>
2019-03-29 17:43:53 +0500

    Button added

---
+++ VoidTime/GUI/Controls/Button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace VoidTime.GUI
+{
+    public class Button : GUIControl
+    {
+        public string Text { get; set; }
+        public FontAtlas Font { get; set; }
+        public float FontSize { get; set; }
+
+        public override List<IDrawData> Draw()
+        {
+            var data = new List<IDrawData>();
+
+            if (!visible)
+                return data;
+
+            if (Background != Color.Empty)
+            {
+                data.Add(new RectangleDrawData
+                {
+                    Color = Background,
+                    DrawPriority = DrawIndex,
+                    Mask = Mask,
+                    Points = LocationSizeConverter.ToVector2DPoints(Location,Size)
+                });
+            }
+            if (!string.IsNullOrWhiteSpace(Text) || Font == null)
+            {
+                data.Add(TextRenderer.GetTextData(Text, Font, FontSize, new RectangleF(Location, Size),
+                    DrawIndex));
+            }
+            return data;
+        }
+    }
+}
