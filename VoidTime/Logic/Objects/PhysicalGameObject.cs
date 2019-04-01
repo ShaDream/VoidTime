67871429e0044ec8f892bc6a67506307c5201ec0
ShaDream <russianboysasha@yandex.ru>
2019-04-01 22:27:20 +0500

    Add PhysicalGameObject

---
+++ VoidTime/Logic/Objects/PhysicalGameObject.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Box2DSharp.Collision.Collider;
+using Box2DSharp.Dynamics;
+using Box2DSharp.Dynamics.Contacts;
+
+namespace VoidTime
+{
+    public abstract class PhysicalGameObject : GameObject
+    {
+        public const float ScaleFactor = 0.001F;
+
+        private Vector2D position;
+
+        public Body Body { get; set; }
+        public List<Fixture> Fixtures { get; set; } = new List<Fixture>();
+
+        public override Vector2D Position
+        {
+            get
+            {
+                if (Body == null)
+                    return position;
+
+                var pos = Body.GetPosition();
+                return new Vector2D(pos.X / ScaleFactor, pos.Y / ScaleFactor);
+            }
+            set
+            {
+                if (Body == null)
+                    position = value;
+                else
+                    PhysicsPosition = new Vector2(value.X * ScaleFactor, value.Y * ScaleFactor);
+            }
+        }
+
+        private Vector2 PhysicsPosition
+        {
+            get => Body.GetPosition();
+            set => Body.SetTransform(value, Body.GetAngle());
+        }
+
+        public virtual void BeginContact(Contact contact) { }
+
+        public virtual void EndContact(Contact contact) { }
+
+        public virtual void PreSolve(Contact contact, in Manifold oldManifold) { }
+
+        public virtual void PostSolve(Contact contact, in ContactImpulse impulse) { }
+
+        public static Vector2 ConvertToPhysicsVector(Vector2D vector)
+        {
+            return new Vector2(vector.X * ScaleFactor, vector.Y * ScaleFactor);
+        }
+
+        public void SetLinearVelocity(Vector2D velocity)
+        {
+            Body.SetLinearVelocity(new Vector2(velocity.X * ScaleFactor, velocity.Y * ScaleFactor));
+        }
+
+        public void ApplyLinearImpulseToCenter(Vector2D velocity)
+        {
+            Body.ApplyLinearImpulseToCenter(new Vector2(velocity.X * ScaleFactor, velocity.Y * ScaleFactor), true);
+        }
+
+        public abstract void CreatePhysics(World world);
+
+        protected BodyDef CreateBodyDef()
+        {
+            return new BodyDef {UserData = this};
+        } 
+
+        public void DeletePhysics()
+        {
+            foreach (var fixture in Fixtures)
+                Body.DestroyFixture(fixture);
+            Fixtures.Clear();
+            position = Position;
+            Body.World.DestroyBody(Body);
+            Body = null;
+        }
+
+        public override void Destoy()
+        {
+            DeletePhysics();
+            base.Destoy();
+        }
+    }
+}
