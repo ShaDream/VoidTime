276091fcf48abef0c860c73edd439b44903ccdea
ShaDream <russianboysasha@yandex.ru>
2019-04-20 18:20:29 +0500

    Change Label to EnterPanel and add aditional function to change size method

---
+++ VoidTime/GUI/EnterPanel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VoidTime.GUI
+{
+    public class EnterPanel : BasicGameWindow
+    {
+        public EnterPanel(MainForm form, Window owner)
+        {
+            this.form = form;
+            this.owner = owner;
+            window = new Label
+            {
+                Text = "Press F",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font(new FontFamily("Arial"), 24),
+                ForeColor = Color.White,
+                Size = new Size(150, 50),
+                Location = new Point(900, 800)
+            };
+        }
+
+        protected override void Show()
+        {
+            form.BeginInvoke(new Action(() =>
+            {
+                form.Controls.Add(window);
+                window.BringToFront();
+            }));
+        }
+
+        protected override void Hide()
+        {
+            form.BeginInvoke(new Action(() =>
+            {
+                form.Controls.Remove(window);
+            }));
+        }
+
+        public override void Switch()
+        {
+            if (isShow)
+                Hide();
+            else
+                Show();
+            isShow = !isShow;
+        }
+    }
+}
