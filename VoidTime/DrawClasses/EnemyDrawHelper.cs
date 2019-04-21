4934cf6672aaa4cfc95cd1e402e4fc390f0714e4
ShaDream <russianboysasha@yandex.ru>
2019-04-21 22:59:46 +0500

    Change Enemy to MapEnemy

--- VoidTime/DrawClasses/EnemyDrawHelper.cs
+++ VoidTime/DrawClasses/EnemyDrawHelper.cs
@@ -9,13 +9,13 @@ namespace VoidTime
     public class EnemyDrawHelper : IDrawable
     {
         private readonly Texture enemy = new Texture();
-        public Type GameObjectType { get; } = typeof(Enemy);
+        public Type GameObjectType { get; } = typeof(MapEnemy);
 
         public void DrawObject(ObjectOnDisplay obj, OpenGL gl)
         {
             enemy.Bind(gl);
             var size = new Size(80, 80);
-            DrawHelper.Draw(obj, gl, size, ((Enemy)obj.GameObject).Angle);
+            DrawHelper.Draw(obj, gl, size, ((MapEnemy)obj.GameObject).Angle);
         }
 
         public void Init(OpenGL gl)
