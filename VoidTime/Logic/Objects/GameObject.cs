9821d1b23b4fbc4c9bff6358d4b396f24440315e
ShaDream <russianboysasha@yandex.ru>
2019-05-01 19:38:19 +0500

    Delete name field GameObject

--- VoidTime/Logic/Objects/GameObject.cs
+++ VoidTime/Logic/Objects/GameObject.cs
@@ -5,7 +5,6 @@ namespace VoidTime
 {
     public class GameObject
     {
-        public string NameObject { get; set; }
         public virtual Vector2D Position { get; set; }
         public virtual Size Size { get; set; }
         public byte DrawingPriority { get; set; }
